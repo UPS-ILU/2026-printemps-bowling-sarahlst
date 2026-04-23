bb27d65e4fd1b6ecf4049b36bb2136bcbbb64fde
sarahlst <laouissetsarah2021@gmail.com>
2026-04-23 15:55:15 +0200

    Niveau2

--- src/bowling/Bowling.java
+++ src/bowling/Bowling.java
@@ -1,16 +1,40 @@
 package bowling;
 
 public class Bowling {
-	
-	int scoreTotal =0;
+	int[] rolls = new int[21];
+	// on vas enrigistrer le resultat de chaque lancer
+	int indice = 0;
+
 	public void roll(int nbQuilles) {
-		scoreTotal = scoreTotal + nbQuilles;
+		rolls[indice++] = nbQuilles;
 	}
-	public int score()
-	{
-		return scoreTotal;
+
+	public int score() {
+		int score = 0;
+		int i = 0;
+
+		for (int frame = 0; frame < 10; frame++) {
+			if (isStrike(i)) {
+				score += 10 + rolls[i + 1]+rolls[i + 2]; // 10 + bonus du lancer suivant
+				i += 1;
+			}
+			if (isSpare(i)) {
+				score += 10 + rolls[i + 2]; // 10 + bonus du lancer suivant
+				i += 2;
+			} else {
+				score += rolls[i] + rolls[i + 1]; // Score normal
+				i += 2;
+			}
+		}
+		return score;
 	}
-	
+
+	private boolean isSpare(int i) {
+		return rolls[i] + rolls[i + 1] == 10;
+	}
+	private boolean isStrike(int i){
+		return rolls[i] == 10;
 		
+	}
 
 }
--- test/bowling/BowlingTest.java
+++ test/bowling/BowlingTest.java
@@ -51,11 +51,63 @@ class BowlingTest {
 		assertEquals(30, game.score());
 
 	}
-	
-	/*@Test 
+
+	@Test
 	void test5() {
-		// on roll 7 puis 3 on (2 lancee) et on fait tomber 10 quilles 
-		// 
+		// on roll 7 puis 3 on (2 lancee) et on fait tomber 10 quilles
+		game.roll(7);
+		game.roll(3);
+		game.roll(4);
+		for (int i = 0; i < 17; i++) {
+			game.roll(0);
+		}
+		assertEquals(18, game.score());
+	}
+
+	@Test
+	void test6() {
+		// on roll 10 et on roll 3 puis on rolle 4
+		// 19 lancees 
+		game.roll(10);
+		game.roll(3);
+		game.roll(4);
+		for (int i = 0; i <16; i++) {
+			game.roll(0);
+		}
+		assertEquals(24, game.score());
+
+	}
+	/*void test7() {
+		game.roll(1); //roll[0]=1
+		game.roll(2);  //roll[1]=2
+		
+		game.roll(10); //roll[2]=10//ya pas de deuxieme lance
+		
+		game.roll(0); 
+		game.roll(10);
+		
+		game.roll(4);
+		game.roll(2);
+		
+		game.roll(0);
+		game.roll(10);
+		
+		game.roll(6);
+		game.roll(2);
+		
+		game.roll(0);
+		game.roll(10);
+		
+		game.roll(6);
+		game.roll(4);
+		
+		game.roll(8);
+		game.roll(2);
+		
+		game.roll(2);
+		game.roll(7);
+		assertEquals(122,game.score());
+		
 	}*/
 
 }
